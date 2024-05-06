fb156152a85e91df76a729a427bf836204e8d8d7
Ubuntu <NPCI-VM-Model@NPCI-VM-Model.oxnw1mhff3bu3jxqiygbgrvyve.rx.internal.cloudapp.net>
2024-05-06 10:10:55 +0000

    uncommented the Assamese Lang

--- src/inverse_text_normalization/run_predict.py
+++ src/inverse_text_normalization/run_predict.py
@@ -9,7 +9,7 @@ from inverse_text_normalization.ta.run_predict import inverse_normalize_text as 
 from inverse_text_normalization.bn.run_predict import inverse_normalize_text as bn_itn
 from inverse_text_normalization.ml.run_predict import inverse_normalize_text as ml_itn
 from inverse_text_normalization.ori.run_predict import inverse_normalize_text as or_itn
-from inverse_text_normalization.asm.run_predict import inverse_normalize_text as as_itn
+# from inverse_text_normalization.asm.run_predict import inverse_normalize_text as as_itn
 from inverse_text_normalization.kn.run_predict import inverse_normalize_text as kn_itn
 
 def format_numbers_with_commas(sent, lang):
@@ -100,11 +100,11 @@ def inverse_normalize_text(text_list, lang):
         itn_results_formatted = [format_numbers_with_commas(sent=sent, lang='hi') for sent in itn_results]
         return itn_results_formatted
 
-    elif lang == 'as':
+    # elif lang == 'as':
 
-        itn_results = as_itn(text_list)
-        itn_results_formatted = [format_numbers_with_commas(sent=sent, lang='hi') for sent in itn_results]
-        return itn_results_formatted
+    #     itn_results = as_itn(text_list)
+    #     itn_results_formatted = [format_numbers_with_commas(sent=sent, lang='hi') for sent in itn_results]
+    #     return itn_results_formatted
 
     
     elif lang == 'kn':
