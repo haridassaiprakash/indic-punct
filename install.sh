b0696e834dc3ba66f27b175b87314883b4b54330
Haridas Sai Prakash <84653703+haridassaiprakash@users.noreply.github.com>
2024-05-13 13:11:15 +0530

    reverting to previous version

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-apt-get update && apt-get install -y libsndfile1 ffmpeg
+sudo apt-get update && apt-get install -y libsndfile1 ffmpeg
 pip install Cython
-# conda install libgcc gmp
+conda install libgcc gmp
 pip install nemo_toolkit['all']
 
-# conda install -c conda-forge pynini==2.1.4
+conda install -c conda-forge pynini==2.1.4
