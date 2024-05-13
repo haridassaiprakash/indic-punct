95567ae928e6e700ce57678b1e4efa2303cecebb
Haridas Sai Prakash <84653703+haridassaiprakash@users.noreply.github.com>
2024-05-13 11:44:21 +0530

    modified for deployment

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-sudo apt-get update && apt-get install -y libsndfile1 ffmpeg
+apt-get update && apt-get install -y libsndfile1 ffmpeg
 pip install Cython
-conda install libgcc gmp
+# conda install libgcc gmp
 pip install nemo_toolkit['all']
 
-conda install -c conda-forge pynini==2.1.4
+# conda install -c conda-forge pynini==2.1.4
